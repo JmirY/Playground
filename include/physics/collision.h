8de925fb24a28c54beacc6ee65c65998e1b68c6f
jmiry <yongseokj2@gmail.com>
2021-02-17 16:22:44 +0900

    Contact 클래스가 충돌 처리를 하도록 변경

--- include/physics/collision.h
+++ include/physics/collision.h
@@ -66,13 +66,28 @@ namespace physics
     };
 
     /* 충돌 정보를 저장하는 구조체. 충돌 처리에 활용된다 */
-    struct Contact
+    class Contact
     {
+    public:
+        friend class CollisionDetector;
+
+    private:
         RigidBody* bodies[2];
         Vector3 normal;
         Vector3 contactPoint;
         float penetration;
         float restitution;
+
+    public:
+        /* 속도 변화 & 겹침 처리를 수행한다 */
+        void resolve();
+    
+    private:
+        /* 충돌 후 두 물체의 속도를 계산하고 적용한다 */
+        void resolveVelocity();
+
+        /* 두 물체 사이의 겹침을 처리한다 */
+        void resolvePenetration();
     };
 
     /* 두 오브젝트 사이에 충돌이 일어났을 때 아래의 탄성 계수를 사용해 충돌 정보를 작성한다 */
@@ -136,13 +151,6 @@ namespace physics
             const Vector3& contactNormal,
             int minPenetrationAxisIdx
         );
-    };
-
-    class CollisionResolver
-    {
-    public:
-        static void resolveVelocity(Contact*);
-        static void resolvePenetration(Contact*);
     };
 } // namespace physics
 
--- src/physics/collision.cpp
+++ src/physics/collision.cpp
@@ -25,6 +25,114 @@ PlaneCollider::PlaneCollider(Vector3 _normal, float _offset)
     offset = _offset;
 }
 
+void Contact::resolve()
+{
+    resolveVelocity();
+    resolvePenetration();
+}
+
+void Contact::resolveVelocity()
+{
+    Vector3 contactPointFromCenter1 = contactPoint - bodies[0]->getPosition();
+    Vector3 contactPointFromCenter2;
+    if (bodies[1] != nullptr)
+        contactPointFromCenter2 = contactPoint - bodies[1]->getPosition();
+
+    /* 두 물체의 충돌점의 속도를 계산한다 */
+    Vector3 contactPointVelocity1 = bodies[0]->getVelocity() +
+        bodies[0]->getRotation().cross(contactPointFromCenter1);
+
+    Vector3 contactPointVelocity2;
+    if (bodies[1] != nullptr)
+    {
+        contactPointVelocity2 = bodies[1]->getVelocity() +
+            bodies[1]->getRotation().cross(contactPointFromCenter2);
+    }
+        
+    /* 상대 속도를 계산한다 */
+    Vector3 relativeVelocity = contactPointVelocity2 - contactPointVelocity1;
+
+    /* 두 물체의 역질량의 합을 계산한다 */
+    float totalInverseMass = bodies[0]->getInverseMass();
+    if (bodies[1] != nullptr)
+        totalInverseMass += bodies[1]->getInverseMass();
+    
+    /* 역질량의 합이 0 이라면
+        두 물체의 질량이 모두 무한대이므로 함수를 종료한다 */
+    if (totalInverseMass == 0.0f)
+        return;
+    
+    /* 충격량의 크기를 계산한다 */
+    float numerator = (relativeVelocity * (1.0f + restitution)).dot(normal);
+
+    Vector3 termInDenominator1 =
+        (bodies[0]->getInverseInertiaTensorWorld() * (contactPointFromCenter1.cross(normal * -1.0f))).cross(contactPointFromCenter1);
+    Vector3 termInDenominator2;
+    if (bodies[1] != nullptr) {
+        termInDenominator2 =
+            (bodies[1]->getInverseInertiaTensorWorld() * (contactPointFromCenter2.cross(normal * -1.0f))).cross(contactPointFromCenter2);
+    }
+    float denominator = totalInverseMass +
+        (termInDenominator1 + termInDenominator2).dot(normal * -1.0f);
+
+    float impulse = numerator / denominator;
+    Vector3 impulseVector = normal * impulse;
+
+    /* 두 물체의 선속도를 갱신한다 */
+    bodies[0]->setVelocity(
+        bodies[0]->getVelocity() + impulseVector * bodies[0]->getInverseMass()
+    );
+
+    if (bodies[1] != nullptr)
+    {
+        bodies[1]->setVelocity(
+            bodies[1]->getVelocity() - impulseVector * bodies[1]->getInverseMass()
+        );
+    }
+
+    /* 두 물체의 각속도를 갱신한다 */
+    Vector3 contactPointFromCenter = contactPoint - bodies[0]->getPosition();
+    bodies[0]->setRotation(
+        bodies[0]->getRotation() + bodies[0]->getInverseInertiaTensorWorld() * impulse * (contactPointFromCenter.cross(normal))
+    );
+
+    if (bodies[1] != nullptr)
+    {
+        contactPointFromCenter = contactPoint - bodies[1]->getPosition();
+        bodies[1]->setRotation(
+            bodies[1]->getRotation() - bodies[1]->getInverseInertiaTensorWorld() * impulse * (contactPointFromCenter.cross(normal))
+        );
+    }
+}
+
+/* linear projection */
+void Contact::resolvePenetration()
+{
+    /* 두 물체의 역질량의 합을 계산한다 */
+    float totalInverseMass = bodies[0]->getInverseMass();
+    if (bodies[1] != nullptr)
+        totalInverseMass += bodies[1]->getInverseMass();
+
+    /* 역질량의 합이 0 이면
+        두 물체가 무한의 질량을 가지는 것이므로 함수를 종료한다 */
+    if (totalInverseMass == 0.0f)
+        return;
+
+    float penetrationPerInverseMass = penetration / totalInverseMass;
+
+    /* 두 물체의 위치를 질량에 반비례하여 조정한다 */
+    Vector3 deltaPosition = normal * penetrationPerInverseMass;
+    bodies[0]->setPosition(
+        bodies[0]->getPosition() + deltaPosition * bodies[0]->getInverseMass()
+    );
+    if (bodies[1] != nullptr)
+    {
+        bodies[1]->setPosition(
+            bodies[1]->getPosition() - deltaPosition * bodies[1]->getInverseMass()
+        );
+    }
+}
+
 bool CollisionDetector::sphereAndBox(
     std::vector<Contact*>& contacts,
     const SphereCollider& sphere,
@@ -444,111 +552,4 @@ Vector3 CollisionDetector::calcContactPointOnLine(
 
     /* 두 점의 중간 지점을 반환한다 */
     return (closestPointOne + closestPointTwo) * 0.5f;
-}
-
-void CollisionResolver::resolveVelocity(Contact* contact)
-{
-    RigidBody* body1 = contact->bodies[0];
-    RigidBody* body2 = contact->bodies[1];
-
-    Vector3 contactPointFromCenter1 = contact->contactPoint - body1->getPosition();
-    Vector3 contactPointFromCenter2;
-    if (body2 != nullptr)
-        contactPointFromCenter2 = contact->contactPoint - body2->getPosition();
-
-    /* 두 물체의 충돌점의 속도를 계산한다 */
-    Vector3 contactPointVelocity1 = body1->getVelocity() +
-        body1->getRotation().cross(contactPointFromCenter1);
-
-    Vector3 contactPointVelocity2;
-    if (body2 != nullptr)
-    {
-        contactPointVelocity2 = body2->getVelocity() +
-            body2->getRotation().cross(contactPointFromCenter2);
-    }
-        
-    /* 상대 속도를 계산한다 */
-    Vector3 relativeVelocity = contactPointVelocity2 - contactPointVelocity1;
-
-    /* 두 물체의 역질량의 합을 계산한다 */
-    float totalInverseMass = body1->getInverseMass();
-    if (body2 != nullptr)
-        totalInverseMass += body2->getInverseMass();
-    
-    /* 역질량의 합이 0 이라면
-        두 물체의 질량이 모두 무한대이므로 함수를 종료한다 */
-    if (totalInverseMass == 0.0f)
-        return;
-    
-    /* 충격량의 크기를 계산한다 */
-    float numerator = (relativeVelocity * (1.0f + contact->restitution)).dot(contact->normal);
-
-    Vector3 termInDenominator1 =
-        (body1->getInverseInertiaTensorWorld() * (contactPointFromCenter1.cross(contact->normal * -1.0f))).cross(contactPointFromCenter1);
-    Vector3 termInDenominator2;
-    if (body2 != nullptr) {
-        termInDenominator2 =
-            (body2->getInverseInertiaTensorWorld() * (contactPointFromCenter2.cross(contact->normal * -1.0f))).cross(contactPointFromCenter2);
-    }
-    float denominator = totalInverseMass +
-        (termInDenominator1 + termInDenominator2).dot(contact->normal * -1.0f);
-
-    float impulse = numerator / denominator;
-    Vector3 impulseVector = contact->normal * impulse;
-
-    /* 두 물체의 선속도를 갱신한다 */
-    body1->setVelocity(
-        body1->getVelocity() + impulseVector * body1->getInverseMass()
-    );
-
-    if (body2 != nullptr)
-    {
-        body2->setVelocity(
-            body2->getVelocity() - impulseVector * body2->getInverseMass()
-        );
-    }
-
-    /* 두 물체의 각속도를 갱신한다 */
-    Vector3 contactPointFromCenter = contact->contactPoint - body1->getPosition();
-    body1->setRotation(
-        body1->getRotation() + body1->getInverseInertiaTensorWorld() * impulse * (contactPointFromCenter.cross(contact->normal))
-    );
-
-    if (body2 != nullptr)
-    {
-        contactPointFromCenter = contact->contactPoint - body2->getPosition();
-        body2->setRotation(
-            body2->getRotation() - body2->getInverseInertiaTensorWorld() * impulse * (contactPointFromCenter.cross(contact->normal))
-        );
-    }
-}
-
-void CollisionResolver::resolvePenetration(Contact* contact)
-{
-    RigidBody* body1 = contact->bodies[0];
-    RigidBody* body2 = contact->bodies[1];
-
-    /* 두 물체의 역질량의 합을 계산한다 */
-    float totalInverseMass = body1->getInverseMass();
-    if (body2 != nullptr)
-        totalInverseMass += body2->getInverseMass();
-
-    /* 역질량의 합이 0 이면
-        두 물체가 무한의 질량을 가지는 것이므로 함수를 종료한다 */
-    if (totalInverseMass == 0.0f)
-        return;
-
-    float penetration = contact->penetration / totalInverseMass;
-
-    /* 두 물체의 위치를 질량에 반비례하여 조정한다 */
-    Vector3 deltaPosition = contact->normal * penetration;
-    body1->setPosition(
-        body1->getPosition() + deltaPosition * body1->getInverseMass()
-    );
-    if (body2 != nullptr)
-    {
-        body2->setPosition(
-            body2->getPosition() - deltaPosition * body2->getInverseMass()
-        );
-    }
 }
